d4615feaae4644802a4934d7b34572491348cba3
Pranav Sharma <sharmapranav@google.com>
2024-08-01 17:16:56 +0000

    Add gradle task to generate executable JAR

--- sample-apps/java/app/build.gradle.kts
+++ sample-apps/java/app/build.gradle.kts
@@ -13,7 +13,30 @@ dependencies {
     implementation("org.slf4j:jul-to-slf4j")
 }
 
+val mainClassName = "com.google.example.app.App"
+
 application {
     // Define the main class for the application.
-    mainClass.set("com.google.example.app.App")
+    mainClass.set(mainClassName)
+}
+
+/**
+ * Create a Fat Jar with all dependencies for easier execution
+ */
+val fatJar = task("fatJar", type = Jar::class) {
+    dependsOn("compileJava")
+    archiveClassifier.set("standalone")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes(Pair("Main-Class", mainClassName))
+    }
+    val sourcesMain = sourceSets.main.get()
+    val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } + sourcesMain.output
+    from(contents)
+    dependsOn(":utilities:jar", ":app:processResources")
+}
+
+tasks.build {
+    dependsOn(fatJar)
 }
